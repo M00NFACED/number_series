c94fc8d23219862f4edde6f37dfcb5cd56defca1
M00NFACED <90569940+M00NFACED@users.noreply.github.com>
2022-03-23 21:13:18 +0300

    Add files via upload

---
+++ number_series/number_series/number_series.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	double sum = 0;
+	double x = 0;
+	for (int n = 2;;n++) {
+		x = 1 / ((n - static_cast<double>(1)) * (n + static_cast < double>(1)));
+		if (x < pow(10, -6)) {
+			break;
+		};
+		sum = sum + x;
+	}
+	std::cout << "Значение числового ряда " << sum;
+}
